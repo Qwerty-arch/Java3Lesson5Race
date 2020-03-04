1de630b18afc6195752032714eaeb5fa17d18aa5
Vlad <oshovsky.vlad@yandex.ru>
2020-03-05 04:11:28 +0800

    commit 2

--- src/multithreading/Tunnel.java
+++ src/multithreading/Tunnel.java
@@ -1,6 +1,13 @@
 package multithreading;
 
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 public class Tunnel extends Stage {
+    private  final int CARS_COUNT = 4;
+    private Semaphore tunnelLimit = new Semaphore(CARS_COUNT / 2);
+    private CyclicBarrier cyclicBarrier;
+
     public Tunnel() {
         this.length = 80;
         this.description = "Тоннель " + length + " метров";
@@ -8,19 +15,21 @@ public class Tunnel extends Stage {
 
     @Override
     public void go(Car c) {
-        try {
-            try {
-                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
-                System.out.println(c.getName() + " начал этап: " + description);
-                Thread.sleep(length / c.getSpeed() * 1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                System.out.println(c.getName() + " закончил этап: " + description);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
+            try {
+                try {
+                    System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                    tunnelLimit.acquire();
+                    System.out.println(c.getName() + " начал этап: " + description);
+                    Thread.sleep(length / c.getSpeed() * 1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println(c.getName() + " закончил этап: " + description);
+                    tunnelLimit.release();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+    }
 }
